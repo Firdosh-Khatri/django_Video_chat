a6ac3b4e524975018e5dde5c43748413ddcba54a
Firdosh-Khatri <firdous.khatri022@gmail.com>
2025-02-21 11:41:27 +0530

    Update asgi.py

--- videochat/videochat/asgi.py
+++ videochat/videochat/asgi.py
@@ -15,7 +15,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack 
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "videochat.settings")
 
 application = ProtocolTypeRouter({
         "http": get_asgi_application(),
